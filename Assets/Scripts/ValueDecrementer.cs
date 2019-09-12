5ef146fecee916653266a03a3fd13576857b7f11
Taylor Grote <grotese@gmail.com>
2019-09-12 17:02:48 -0500

    Initial Commit

---
+++ Assets/Scripts/ValueDecrementer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ValueDecrementer : MonoBehaviour
+{
+    [SerializeField] ValueAsset _value;
+
+    public void Decrement()
+    {
+        --_value.Value;
+    }
+}
---
+++ Assets/Scripts/ValueTextRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ValueTextRenderer : MonoBehaviour
+{
+    [SerializeField] ValueAsset _value;
+
+    Text _text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<Text>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _text.text = _value.Value.ToString();
+    }
+}
